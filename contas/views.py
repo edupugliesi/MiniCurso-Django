69ce45fd44454df82dd96b41b567b0ad09f0e50f
Eduardo Pugliesi <70285858+edupugliesi@users.noreply.github.com>
2023-01-31 14:08:43 -0300

    31/01 - MiniCurso concluído

--- contas/views.py
+++ contas/views.py
@@ -14,16 +14,39 @@ def home(request):
     
     return render(request, 'contas/home.html', data)
 
+
 def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request, 'contas/listagem.html', data)
 
+
 def nova_transacao(request):
+    data = {}
     form = TransacaoForm(request.POST or None)
 
     if form.is_valid():
         form.save()
         return redirect('listagem')
-    return render(request, 'contas/form.html', {'form': form})
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
 
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('listagem')
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -18,12 +18,14 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, listagem, nova_transacao
+from contas.views import home, listagem, nova_transacao, update, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', home),
     path('nova', nova_transacao, name='url_nova'),
+    path('update/<int:pk>', update, name='url_update'),
+    path('delete/<int:pk>', delete, name='url_delete'),
     path('', listagem, name='listagem')
 ]
  
